ce6827de1fcad5a839eec7cf3b1e002cfdaade34
Lola\Victor <victorguerin19@gmail.com>
2022-02-16 15:18:09 -0500

    da

--- Noeud.h
+++ Noeud.h
@@ -18,30 +18,19 @@ public:
 
 template<typename TType> inline
 Noeud<TType>::Noeud() {
-<<<<<<< HEAD
 	cout << "Creation Noeud sans paramètre" << endl;
-=======
->>>>>>> b51fa1f5e600e5027e3983b3866e35e8c7f645b1
 	Suivant = nullptr;
 }
 
 template<typename TType> inline
 Noeud<TType>::Noeud(const TType& _info, Noeud<TType>* suiv) {
-<<<<<<< HEAD
 	cout << "Creation Noeud avec paramètres" << endl;
-=======
->>>>>>> b51fa1f5e600e5027e3983b3866e35e8c7f645b1
 	info = _info;
 	Suivant = suiv;
 }
 
 template<typename TType> inline
 Noeud<TType>::Noeud(Noeud<TType>* suiv) {
-<<<<<<< HEAD
-
-=======
-	info = TType();
->>>>>>> b51fa1f5e600e5027e3983b3866e35e8c7f645b1
 	Suivant = suiv;
 }
 
